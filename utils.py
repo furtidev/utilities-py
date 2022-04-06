509078a7b9de09a41940d7a6d4dc7937f875a0e3
Isfer Hossain <midrashahil123@gmail.com>
2022-04-07 01:08:01 +0600

    Function based structure instead of class-based

--- utils.py
+++ utils.py
@@ -2,11 +2,7 @@
 import json
 import requests
 
-# Main Class
-class utils:
-	def __init__(self) -> None:
-		pass
-	
-	def use_rest_api(self, url: str):
-		response = requests.get(url)
-		return response.json()
+# Functions	
+def use_rest_api(url: str):
+	response = requests.get(url)
+	return response.json()
